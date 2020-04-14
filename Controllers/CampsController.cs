cc0a0d6dac3f4a9660e6a80e374b1ab3d1fdfec2
Gregor Biswanger <gregor.biswanger@web-enliven.de>
2020-04-14 23:14:26 +0200

    Get entities from the database

--- Controllers/CampsController.cs
+++ Controllers/CampsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CoreCodeCamp.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +12,27 @@ namespace AspRestApiWorkshop.Controllers
     [ApiController]
     public class CampsController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult GetCamps()
-        {
-            if (true) return NotFound();
+        private readonly ICampRepository _campRepository;
 
-            return Ok(new { Moniker = "DWX2020", Name = "Developer Week 2020" });
+        public CampsController(ICampRepository campRepository)
+        {
+            _campRepository = campRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var results = await _campRepository.GetAllCampsAsync();
+
+                return Ok(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+
         }
     }
 }
